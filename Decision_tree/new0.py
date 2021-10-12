d6c603bc423a0dc3f7977e9e2188df481b41387a
anirban-1009 <anirban9293@gmail.com>
2021-10-12 16:59:08 +0530

    04.59

--- Decision_tree/new0.py
+++ Decision_tree/new0.py
@@ -4,13 +4,17 @@ class Entropy:
     def __init__(self):
         self
 
+    def ginni(self, p):
+        g = 2*p*(1-p)
+        return g
+
     def ent(self,p):
         e = -(p * log(p, 2) + (1-p) * log((1-p), 2))
         return e
 
     def p(self,n,t):
         p = n/(n+t)
-        return self.ent(p)
+        return p
 
 m = Entropy()
 print(m.p(45, 40))
