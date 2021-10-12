3f792c931411466a1f05a3be00bc62dbd5b7664c
anirban-1009 <anirban9293@gmail.com>
2021-10-12 10:52:48 +0530

    1210

--- Decision_tree/new0.py
+++ Decision_tree/new0.py
@@ -1,1 +1,16 @@
-Entropy =  -(0.401 * log(0.401) + (1-0.401) * log(1-0.401))
+from math import log
+
+class Entropy:
+    def __init__(self):
+        self
+
+    def ent(self,p):
+        e = -(p * log(p, 2) + (1-p) * log((1-p), 2))
+        return e
+
+    def p(self,n,t):
+        p = n/(n+t)
+        return self.ent(p)
+
+m = Entropy()
+print(m.p(45, 40))
