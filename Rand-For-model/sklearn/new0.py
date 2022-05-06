c65f26b4b6f64b1c603e1224c24834f03552e6ef
anirban-1009 <anirban9293@gmail.com>
2022-05-06 20:40:06 +0530

    Random tree

---
+++ Rand-For-model/sklearn/new0.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+
+cancer_data = load_breast_cancer()
+df = pd.DataFrame(cancer_data['data'], columns=cancer_data['feature_names'])
+df['target'] = cancer_data['target']
+
+x = df[cancer_data.feature_names].values
+y = df['target'].values
+
+print('data dimensions', x.shape)
